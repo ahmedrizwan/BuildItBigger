ebff4740ad13d34d29c8722456d08f235c23df7d
Ahmed Rizwan <ahmedrizwan@outlook.com>
2015-11-16 05:57:42 +0500

    Conversion to ConnectedTests  - Remove Unit Tests  - Remove Robolectric  - Add AsyncTests as ConnectedTest  - Verify observable response

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTests.java
@@ -0,0 +1,42 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.MediumTest;
+
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by ahmedrizwan on 16/11/2015.
+ */
+public class AsyncTests extends ActivityInstrumentationTestCase2<MainActivity> {
+    Observables mObservables;
+
+    public AsyncTests() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() {
+        mObservables = new Observables();
+    }
+
+    public AsyncTests(final Class<MainActivity> activityClass) {
+        super(activityClass);
+    }
+
+    @MediumTest
+    public void testAsyncReturnsJoke() {
+        String jokeString = "";
+        try {
+            jokeString = mObservables.getJokeObservable(mObservables.getMyApi())
+                    .subscribeOn(Schedulers.newThread())
+                    .toBlocking()
+                    .first();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        //Assert that a string is returned
+        assertTrue(jokeString.length() > 0);
+        System.out.println("Joke Returned : " + jokeString);
+    }
+}
